4cf899da5166d6d9d3b536e41fa6659704541f7f
Emad Khan <emadkhan@brandeis.edu>
2016-11-19 20:35:15 -0500

    Changed reducer to write
    profession <lemma, count>. Also used BufferedReader instead of Scanner
    in loading professions.

--- src/main/java/cosi129/pa3/NaiveBayes.java
+++ src/main/java/cosi129/pa3/NaiveBayes.java
@@ -1,12 +1,11 @@
 package cosi129.pa3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.ArrayList;
 
@@ -35,32 +34,34 @@ public class NaiveBayes {
         
         // Setup builds hash map of names-to-professions before mapper jobs
         protected void setup(Context context) throws IOException, InterruptedException {
-            // Get professions file as input stream from jar resources
-            InputStream in = getClass().getResourceAsStream(PROFESSIONS_FILE);
-            Scanner sc = new Scanner(new InputStreamReader(in));
             professionMapping = new HashMap<String, List<String>>();
-           
-            // Scan through file line by line
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                String[] split = line.split(":");
-
-                
-                String name = "";
-                // Separate names and their professions
-                if(split.length > 0) name = split[0].trim();
-                String[] professions = new String[1];
-                if(split.length > 1 ) professions = split[1].split(","); 
-                List<String> profList = new ArrayList<String>();
-               
-                for (String prof : professions) {
-                    if(prof != null) profList.add(prof.trim());
-                }
-               
-                professionMapping.put(name, profList);
-            }
-            sc.close();
-        }
+            BufferedReader bufferedReader = null;
+    		try {
+                bufferedReader =  new BufferedReader(new FileReader(PROFESSIONS_FILE));
+    			String line;
+    			while ((line = bufferedReader.readLine()) != null) {
+                    String[] split = line.split(":");
+                    String name = "";
+                    // Separate names and their professions
+                    if(split.length > 0) name = split[0].trim();
+                    String[] professions = new String[1];
+                    if(split.length > 1 ) professions = split[1].split(","); 
+                    List<String> profList = new ArrayList<String>();
+                   
+                    for (String prof : professions) {
+                        if(prof != null) profList.add(prof.trim());
+                    }
+                    professionMapping.put(name, profList);
+    			}
+    			
+    		} finally {
+    			try {
+    				if (bufferedReader != null) {
+    					bufferedReader.close();
+    				}
+    			} catch (IOException e) {}
+    		}
+    	}
         
 		public void map(Text articleId, Text indices, Context context) throws IOException, InterruptedException {
 			// Check if this person has profession classified, if not ignore
@@ -81,25 +82,14 @@ public class NaiveBayes {
 	}
 	
 	public static class ProfessionReducer extends Reducer<Text, Text, Text, Text> {
-		public void reduce(Text profession, Iterable<Text> terms, Context context) 
-				throws IOException, InterruptedException {
-			
-			System.out.println("Profession = " + profession);
-			
+		public void reduce(Text profession, Iterable<Text> terms, Context context) throws IOException, InterruptedException {
+			HashMap<String, Integer> lemmaCounts = new HashMap<String, Integer>();
 			Iterator<Text> iter = terms.iterator();
-			Configuration conf = context.getConfiguration();
-			String professionFormated = profession.toString().replaceAll(" ", "_");
-			StringBuilder docLems = new StringBuilder();
-			// Since the SequenceFile API does not support writing to existing file in Hadoop 2.3, we must make multiple .seq files
-			// While normally we would write 1 big .seq files for a profession, heap space limitations prevent this so we must keep flushing to disk
-			int seqFilesWrittenCount = 0;
 			
 			while(iter.hasNext()) {
-				// Unroll the lemma and word count pairs so they can be formatted correctly for mahout
-				// For example, math, 3 ==> math math math
 				String si = iter.next().toString();
 				String[] lemmaAndCount = si.split(",");
-				String lemma = lemmaAndCount[0] + " ";
+				String lemma = lemmaAndCount[0];
 				int lemmaCount = 0;
 				try {
 					lemmaCount = Integer.parseInt(lemmaAndCount[1]);
@@ -107,25 +97,14 @@ public class NaiveBayes {
 					// This means malformed data from the provided lemma index
 					// This is not our problem, so ignore this exception
 				}
-				
-				for (int i = 0; i < lemmaCount; i++){
-					// Set buffer size to 512, this basically means sequence files are maximum length of 512 to prevent heap overflow
-					if (docLems.length() > 512) {
-						// Append the lemma to our buffer, also remove whitespace at end since this is final lemma added
-						// Flush our string buffer to HDFS as a sequence file, and then increment our file count for this profession
-						docLems.append(lemma.trim());
-						URI path = URI.create("seqfiles/" + professionFormated + seqFilesWrittenCount + ".seq");
-						FileSystem fs = FileSystem.get(path, conf);
-						SequenceFile.Writer writer = new Writer(fs, conf, new Path(path), profession.getClass(), Text.class);
-						writer.append(profession, new Text(docLems.toString()));
-						seqFilesWrittenCount++;
-						docLems.setLength(0);
-						writer.close();
-					} else {
-						docLems.append(lemma);
-					}
+				if (lemmaCounts.containsKey(lemma)) {
+					lemmaCounts.put(lemma, lemmaCounts.get(lemma) + lemmaCount);
+				} else {
+					lemmaCounts.put(lemma, lemmaCount);
 				}
 			}
+			StringIntegerList lemmaToWrite = new StringIntegerList(lemmaCounts);
+			context.write(profession, new Text(lemmaToWrite.toString()));
 		}
 	}
 	
